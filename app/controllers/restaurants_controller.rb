bb5138bcbdabca6cfb8df232d1fc0172cec37f0e
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-02-16 13:43:18 +0000

    added faker gem

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,40 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(set_restaurant)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(set_restaurant)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def set_restaurant
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  puts "start creating a restaurant"
+  restaurant = Restautant.create!(
+    name: Faker::FunnyName.two_word_name,
+    address: Faker::Address.street_address,
+    category: Restaurant::CATEGORY.sample,
+  )
+  puts "finished creating restaurant #{restaurant.id}"
+end
